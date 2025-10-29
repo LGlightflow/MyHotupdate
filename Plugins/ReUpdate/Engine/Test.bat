b1de007c40be38b803727f2798cb3b9cbd019366
lglight <2285434325@qq.com>
2025-10-29 23:31:12 +0800

    add some other resources

---
+++ Plugins/ReUpdate/Engine/Test.bat
@@ -0,0 +1,7 @@
+start Binaries\Win64\HotUpdateClient.exe -bTest=true -log
+start Binaries\Win64\OSSObjectStorageClient.exe -bTest=true -log 
+start Binaries\Win64\LocalObjectStorageClient.exe -bTest=true -log 
+start Binaries\Win64\HTTPObjectStorageClient.exe -bTest=true -log 
+start Binaries\Win64\SimpleInstallationProgress.exe -bTest=true -log 
+
+pause
